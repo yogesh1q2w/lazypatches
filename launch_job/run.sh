451fc016b066578aea930bc2fb071cc476827e68
yogesh1q2w <yogesh120699@gmail.com>
2024-12-03 18:11:11 +0100

    run.sh update

--- launch_job/run.sh
+++ launch_job/run.sh
@@ -3,11 +3,11 @@
 #SBATCH --gres=gpu:a100:1 -C a100_80
 #SBATCH --time=00:10:00 
 #SBATCH --job-name=run
-#SBATCH --output=/home/hpc/g102ea/g102ea11/m3-project/lazypatches/eval.log
+#SBATCH --output=/home/hpc/g102ea/<username>/lazypatches/eval.log
 #SBATCH --export=None
 
 module add python
 conda activate m3project
-source env/bin/activate
+source .env/bin/activate
 
 python3 inference/run.py > output.out
