0c13eccc4859bd9812016307aaa07a5ff2dbde67
yogesh1q2w <yogesh120699@gmail.com>
2024-12-15 16:06:14 +0100

    Charades MCQ code cleanup

--- inference/run_action.py
+++ inference/run_action.py
@@ -3,29 +3,36 @@ import torch
 from transformers import Qwen2VLForConditionalGeneration, AutoProcessor
 
 from torch.utils.data import DataLoader
-from dataset.charadesaction import Charades_action
+from dataset.charades_action import CharadesActionMCQ
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-checkpoint_path = "/home/atuin/g102ea/shared/group_10/model_checkpoints/qwen2vl-7b-instruct"
+MODEL_CHECKPOINT_PATH = "/home/atuin/g102ea/shared/group_10/model_checkpoints/qwen2vl-7b-instruct"
+
+ROOT_PATH = "/home/atuin/g102ea/g102ea12/dataset"
+DATASET_PATH = os.path.join(ROOT_PATH, "charades")
+
+if os.path.exists(os.path.join(DATASET_PATH, "charades_mcq.json")):
+    charades_dataset = CharadesActionMCQ(dataset_path=os.path.exists(os.path.join(DATASET_PATH, "charades_mcq.json")), reload=True)
+else:
+    charades_dataset = CharadesActionMCQ(dataset_path=os.path.exists(os.path.join(DATASET_PATH, "charades_mcq.json")),
+                                    videos_path=os.path.join(DATASET_PATH, "/videos/Charades_v1"),
+                                    labels_path=os.path.join(DATASET_PATH, "anotations/Charades/Charades_v1_test.csv"),
+                                    classes_path=os.path.join(DATASET_PATH, "anotations/Charades/Charades_v1_classes.txt"),
+                                    n_wrong_options=4,
+                                    reload=False
+                                    )
+
 
 # Load the model in half-precision on the available device(s)
-model = Qwen2VLForConditionalGeneration.from_pretrained(checkpoint_path, device_map="auto", torch_dtype="auto")
-processor = AutoProcessor.from_pretrained(checkpoint_path, max_pixels=202500)
+model = Qwen2VLForConditionalGeneration.from_pretrained(MODEL_CHECKPOINT_PATH, device_map="auto", torch_dtype="auto")
+processor = AutoProcessor.from_pretrained(MODEL_CHECKPOINT_PATH)
 
 print("Loading model complete")
 
-ROOT_PATH = "/home/atuin/g102ea/g102ea12/datasets"
-DATASET_PATH = os.path.join(ROOT_PATH, "charades")
-
-charades_dataset = Charades_action(videos_path=os.path.join(DATASET_PATH, "/videos/Charades_v1"),
-                                labels_path=os.path.join(DATASET_PATH, "anotations/Charades/Charades_v1_test.csv"),
-                                classes_path=os.path.join(DATASET_PATH, "anotations/Charades/Charades_v1_classes.txt")
-                                )
-
 data_loader = DataLoader(dataset=charades_dataset, batch_size=1, shuffle=True)
 data_loader_iter = iter(data_loader)
-for i in range(3):
+for i in range(5):
     video, question, answer = next(data_loader_iter)
 
     video = video.squeeze(0)
@@ -41,11 +48,12 @@ for i in range(3):
     ]
 
     text_prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
-    inputs = processor(text=[text_prompt], videos=[video], padding=True, return_tensors="pt", do_resize=True, size=140)
+    inputs = processor(text=[text_prompt], videos=[video], padding=True, return_tensors="pt")
     inputs = inputs.to(DEVICE)
 
     output_ids = model.generate(**inputs, max_new_tokens=128)
     generated_ids = [output_ids[len(input_ids):] for input_ids, output_ids in zip(inputs.input_ids, output_ids)]
     output_text = processor.batch_decode(generated_ids, skip_special_tokens=True, clean_up_tokenization_spaces=True)
-    torch.cuda.empty_cache()
     print(output_text)
+    torch.cuda.empty_cache()
+    
