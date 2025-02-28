4056fe9e781e59b9d07e30f9201be4863e0b7e55
Anirudha Poovalakunta <g102ea16@alex1.nhr.fau.de>
2025-02-28 16:06:23 +0100

    launch_job changes"

--- launch_job/run_charades_mcq.sh
+++ launch_job/run_charades_mcq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #
 #SBATCH --gres=gpu:a100:1 -C a100_80
-#SBATCH --time=00:10:00 
+#SBATCH --time=05:00:00 
 #SBATCH --job-name=run
 #SBATCH --output=/home/hpc/g102ea/g102ea16/lazypatches/eval.log
 #SBATCH --export=None
@@ -10,4 +10,4 @@ module add python
 conda activate m3project
 source .env/bin/activate
 
-python3 inference/run.py > output.out
+python3 inference/run_subset_action.py > output.out
