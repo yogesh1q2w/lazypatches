3d6b0ca9e19c0eec2e9d0574e6b859d32551e8f0
Anirudha Poovalakunta <g102ea16@alex1.nhr.fau.de>
2025-03-10 17:58:18 +0100

    rm llm fps

--- launch_job/run_mcq.sh
+++ launch_job/run_mcq.sh
@@ -11,14 +11,12 @@ conda activate m3project
 source .env/bin/activate
 
 # Positional arguments:
-# $1: LLM_FPS
-# $2: RETENTION_RATE
-# $3: SAMPLER_TYPE
-# $4: DATASET
-# $5: HYPERPARAM
-# $6: DROPPING_POSITION
+# $1: RETENTION_RATE
+# $2: SAMPLER_TYPE
+# $3: DATASET
+# $4: HYPERPARAM
+# $5: DROPPING_POSITION
 
-# [LLM_FPS] : [0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2]; CHANGES ONLY FOR BASELINE W/O SAMPLING + ANALYSIS EXP FOR BEST SAMPLING RATE + SAMPLING POSITION
 # [RETENTION_RATE] : [.10, .30, .50, .70, .90] CHANGES FOR ALL EXPERIMENTS BAR BEST SAMPLING RATE EXPERIMENT
 # [SAMPLER_TYPE] : [None, uniform, st_gaussian, km_closest, tb] 
 # [DATASET] : [Charades, PerceptionTest]
@@ -30,25 +28,25 @@ source .env/bin/activate
 
 # Example commands: 
 # --no sampler:
-#       sbatch launch_job/run_mcq.sh 1.0 0 None Charades 0 0 
-#       {Dataset is Charades, Hyperparam is 0, retention_rate is 0.50, llm_fps is 1.0, dropping_position is 0}
+#       sbatch launch_job/run_mcq.sh 0 0 None Charades 0 0 
+#       {Dataset is Charades, Hyperparam is 0, retention_rate is 0.50, dropping_position is 0}
 # --uniform sampler: 
-#       sbatch launch_job/run_mcq.sh 1.0 .50 uniform Charades 0 0 
-#       {Dataset is Charades, Hyperparam is 0, retention_rate is 0.50, llm_fps is 1.0, dropping_position is 0}
+#       sbatch launch_job/run_mcq.sh .50 uniform Charades 0 0 
+#       {Dataset is Charades, Hyperparam is 0, retention_rate is 0.50, dropping_position is 0}
 # --st_gaussian sampler: 
-#       sbatch launch_job/run_mcq.sh 1.0 .50 st_gaussian Charades 1 0 
-#       {Dataset is Charades, Hyperparam is 1, retention_rate is 0.50, llm_fps is 1.0, dropping_position is 0}
+#       sbatch launch_job/run_mcq.sh .50 st_gaussian Charades 1 0 
+#       {Dataset is Charades, Hyperparam is 1, retention_rate is 0.50, dropping_position is 0}
 # --km_closest sampler: 
-#       sbatch launch_job/run_mcq.sh 1.0 .50 km_closest Charades 0.50 0 
-#       {Dataset is Charades, Hyperparam is 0.5, retention_rate is 0.50, llm_fps is 1.0, dropping_position is 0}
+#       sbatch launch_job/run_mcq.sh .50 km_closest Charades 0.50 0 
+#       {Dataset is Charades, Hyperparam is 0.5, retention_rate is 0.50, dropping_position is 0}
 
 TARGET_PATH="/home/atuin/g102ea/shared/group_10/results"
 
-DATASET=$(echo "$1" | tr '[:upper:]' '[:lower:]')
-RETENTION_RATE="$4"
+DATASET=$(echo "$3" | tr '[:upper:]' '[:lower:]')
+RETENTION_RATE="$1"
 SAMPLER_TYPE="$2"
-HYPERPARAM="$5"
-DROPPING_POSITION="$3"
+HYPERPARAM="$4"
+DROPPING_POSITION="$5"
 
 if [ "$SAMPLER_TYPE" = "None" ]; then
     TARGET_PATH="${TARGET_PATH}/baseline"
@@ -62,9 +60,9 @@ fi
 
 retention_percent=$(printf "%.0f" "$(echo "$RETENTION_RATE * 100" | bc -l)")
 
-SUB_FOLDER="${DATASET}_${SAMPLER_TYPE}_${LLM_FPS}_${DROPPING_POSITION}_${retention_percent}%_${HYPERPARAM}"
+SUB_FOLDER="${DATASET}_${SAMPLER_TYPE}_${DROPPING_POSITION}_${retention_percent}%_${HYPERPARAM}"
 TARGET_PATH="${TARGET_PATH}/${SUB_FOLDER}"
 
 mkdir -p "$TARGET_PATH"
 
-python3 inference/run_subset_mcq.py "$1" "$2" "$3" "$4" "$5" "$6" $TARGET_PATH > "$TARGET_PATH/output.out" 2> "$TARGET_PATH/error.log"
+python3 inference/run_subset_mcq.py "$1" "$2" "$3" "$4" "$5" $TARGET_PATH > "$TARGET_PATH/output.out" 2> "$TARGET_PATH/error.log"
