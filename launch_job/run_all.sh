a403dcb21be537432435bddf34134dc5b11eeb6e
aritraM23 <aritramarik2002@gmail.com>
2025-03-12 14:29:37 +0100

    append to job ids

--- launch_job/run_all.sh
+++ launch_job/run_all.sh
@@ -2,9 +2,7 @@
 
 # Create or clear a file to store job IDs
 job_log="job_ids.log"
-echo "Recording job IDs in $job_log"
-echo "Command | Job ID" > "$job_log"
-echo "-----------------" >> "$job_log"
+echo "Appending job IDs to $job_log"
 
 Drop  # Set your dropping position number here
 
@@ -17,7 +15,7 @@ for retention in 0.10 0.30 0.50 0.70 0.90; do
             charades_job_id=$(echo "$charades_output" | awk '{print $NF}')  # Extract the last word (job ID)
             echo "$charades_cmd | $charades_job_id" >> "$job_log"
 
-            # Submit job for PerceptionTest and capture job ID
+            # # Submit job for PerceptionTest and capture job ID
             perception_cmd="sbatch launch_job/run_mcq.sh $retention $sampler PerceptionTest 1 $Drop"
             perception_output=$($perception_cmd)
             perception_job_id=$(echo "$perception_output" | awk '{print $NF}')  # Extract the last word (job ID)
@@ -30,7 +28,7 @@ for retention in 0.10 0.30 0.50 0.70 0.90; do
             charades_job_id=$(echo "$charades_output" | awk '{print $NF}')  
             echo "$charades_cmd | $charades_job_id" >> "$job_log"
 
-            # Submit job for PerceptionTest and capture job ID
+            # # Submit job for PerceptionTest and capture job ID
             perception_cmd="sbatch launch_job/run_mcq.sh $retention $sampler PerceptionTest 0 $Drop"
             perception_output=$($perception_cmd)
             perception_job_id=$(echo "$perception_output" | awk '{print $NF}')  
@@ -43,7 +41,7 @@ for retention in 0.10 0.30 0.50 0.70 0.90; do
             charades_job_id=$(echo "$charades_output" | awk '{print $NF}')  
             echo "$charades_cmd | $charades_job_id" >> "$job_log"
 
-            # Submit job for PerceptionTest and capture job ID
+            # # Submit job for PerceptionTest and capture job ID
             perception_cmd="sbatch launch_job/run_mcq.sh $retention $sampler PerceptionTest 0.5 $Drop"
             perception_output=$($perception_cmd)
             perception_job_id=$(echo "$perception_output" | awk '{print $NF}')  
